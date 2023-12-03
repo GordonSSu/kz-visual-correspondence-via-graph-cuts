352cfa88d2aa9b6785f4c0c2a63175ba7c59120f
Gordon Su <gssu@usc.edu>
2023-12-02 22:26:44 -0500

    Work in progress

--- graph_cuts_stereo.py
+++ graph_cuts_stereo.py
@@ -3,8 +3,6 @@ import maxflow
 import numpy as np
 import random
 
-import matplotlib.pyplot as plt
-
 VAR_ALPHA = -1
 VAR_ABSENT = -2
 NEIGHBORS = [[0, 1], [1, 0]]
@@ -15,52 +13,25 @@ class GraphCutsStereo:
         self.left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY).astype(int)
         self.right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2GRAY).astype(int)
 
-        self.max_disparity = int(0.12 * self.num_cols)
-        self.I_disp = list(range(1, self.max_disparity))
+        self.max_disparity = int(0.15 * self.num_cols)
+        self.I_disp = list(range(self.max_disparity))
         self.done = [False for i in range(len(self.I_disp))]
 
-        window_radius = 3
-        self.d_left = np.full((self.num_rows, self.num_cols), -1) # np.zeros((self.num_rows, self.num_cols), dtype=int)
+        self.d_left = np.full((self.num_rows, self.num_cols), -1)
         self.d_right = np.full((self.num_rows, self.num_cols), -1)
-        # self.initialize_d_left_right(window_radius)
 
         self.K = 30
-        self.lam = 8
+        self.lambda_smooth = 10
+        self.trim_cutoff = 30
         self.curr_energy = float('inf')
         self.constant_additional_cut_cost = 0
         self.vars_A = np.zeros((self.num_rows, self.num_cols), dtype=int)
         self.vars_0 = np.zeros((self.num_rows, self.num_cols), dtype=int)
 
-    def initialize_d_left_right(self, window_radius):
-        window_dimensions = 1 + 2 * window_radius
-
-        left = cv2.copyMakeBorder(self.left_image, window_radius, window_radius, window_radius, window_radius, cv2.BORDER_REFLECT_101)
-        right = cv2.copyMakeBorder(self.right_image, window_radius, window_radius, window_radius, window_radius, cv2.BORDER_REFLECT_101)
-
-        # Determine best correspondence for each pixel
-        for row in range(self.num_rows):
-            for col in range(self.num_cols):
-                template_window = left[row:row + window_dimensions, col:col + window_dimensions]
-                min_ssd = float('inf')
-                disparity = 0
-
-                # Search only along horizontal epipolar line
-                for epipolar_col in range(max(0, col - self.max_disparity), col):
-                    proposed_window = right[row:row + window_dimensions, epipolar_col:epipolar_col + window_dimensions]
-                    ssd = np.einsum('ij,ij', template_window - proposed_window, template_window - proposed_window)
-            
-                    if ssd < min_ssd:
-                        min_ssd = ssd
-                        disparity = col - epipolar_col
-
-                # Set disparity between pixel and its correspondent
-                self.d_left[row][col] = disparity
-                self.d_right[row][col - disparity] = disparity
-
     def reset_state(self):
         self.constant_additional_cut_cost = 0
-        self.vars_A = np.zeros((self.num_rows, self.num_cols))
-        self.vars_0 = np.zeros((self.num_rows, self.num_cols))
+        self.vars_A = np.zeros((self.num_rows, self.num_cols), dtype=int)
+        self.vars_0 = np.zeros((self.num_rows, self.num_cols), dtype=int)
 
     def add_nodes(self, g, alpha):
         num_nodes = 0
@@ -106,26 +77,29 @@ class GraphCutsStereo:
         nodes = g.add_nodes(num_nodes)
         return nodes
 
+    def calculate_D_prime(self, I_p, I_q):
+        return min(self.trim_cutoff, np.abs(I_p - I_q)) ** 2 - self.K
+
     def create_data_occlusion_edges(self, g, alpha):
         for row in range(self.num_rows):
             for col in range(self.num_cols):
                 
                 # Currently active assigment with disparity == alpha
                 if self.vars_A[row][col] == VAR_ALPHA and self.vars_0[row][col] == VAR_ALPHA:
-                    D_prime = np.abs(self.left_image[row][col] - self.right_image[row][col - alpha]) ** 2 - self.K
+                    D_prime = self.calculate_D_prime(self.left_image[row][col], self.right_image[row][col - alpha])
                     self.constant_additional_cut_cost += D_prime
 
                 # Currently inactive assigment with disparity == alpha
                 if self.vars_A[row][col] >= 0:
                     a = self.vars_A[row][col]
-                    D_prime = np.abs(self.left_image[row][col] - self.right_image[row][col - alpha]) ** 2 - self.K
+                    D_prime = self.calculate_D_prime(self.left_image[row][col], self.right_image[row][col - alpha])
                     g.add_tedge(a, D_prime, 0)
 
                 # Currently active assigment with disparity != alpha
                 if self.vars_0[row][col] >= 0:
                     o = self.vars_0[row][col]
                     pixel_disparity = self.d_left[row][col]
-                    D_prime = np.abs(self.left_image[row][col] - self.right_image[row][col - pixel_disparity]) ** 2 - self.K
+                    D_prime = self.calculate_D_prime(self.left_image[row][col], self.right_image[row][col - pixel_disparity])
                     g.add_tedge(o, 0, D_prime)
 
     def add_term2(self, g, a1, a2, E_00, E_01, E_10, E_11):
@@ -145,11 +119,11 @@ class GraphCutsStereo:
                         if self.vars_A[row][col] >= 0 and self.vars_A[neighbor_row][neighbor_col] >= 0:
                             a1 = self.vars_A[row][col]
                             a2 = self.vars_A[neighbor_row][neighbor_col]
-                            V = self.lam
+                            V = self.lambda_smooth
                             
                             if max(np.abs(self.left_image[row][col] - self.left_image[neighbor_row][neighbor_col]), \
                                 np.abs(self.right_image[row][col - alpha] - self.right_image[neighbor_row][neighbor_col - alpha])) < 8:
-                                V = 3 * self.lam
+                                V = 3 * self.lambda_smooth
 
                             self.add_term2(g, a1, a2, 0, V, V, 0)
                             # self.add_term2(g, a2, a1, 0, V, V, 0)
@@ -157,20 +131,20 @@ class GraphCutsStereo:
                         # Case 2: Alpha disparity neighbors: one inactive
                         if self.vars_A[row][col] >= 0 and self.vars_A[neighbor_row][neighbor_col] == VAR_ALPHA:
                             a1 = self.vars_A[row][col]
-                            V = self.lam
+                            V = self.lambda_smooth
 
                             if max(np.abs(self.left_image[row][col] - self.left_image[neighbor_row][neighbor_col]), \
                                 np.abs(self.right_image[row][col - alpha] - self.right_image[neighbor_row][neighbor_col - alpha])) < 8:
-                                V = 3 * self.lam
+                                V = 3 * self.lambda_smooth
 
                             g.add_tedge(a1, 0, V)
                         elif self.vars_A[row][col] == VAR_ALPHA and self.vars_A[neighbor_row][neighbor_col] >= 0:
                             a1 = self.vars_A[neighbor_row][neighbor_col]
-                            V = self.lam
+                            V = self.lambda_smooth
 
                             if max(np.abs(self.left_image[row][col] - self.left_image[neighbor_row][neighbor_col]), \
                                 np.abs(self.right_image[row][col - alpha] - self.right_image[neighbor_row][neighbor_col - alpha])) < 8:
-                                V = 3 * self.lam
+                                V = 3 * self.lambda_smooth
                             
                             g.add_tedge(a1, 0, V)
 
@@ -179,11 +153,12 @@ class GraphCutsStereo:
                         self.d_left[row][col] == self.d_left[neighbor_row][neighbor_col]:
                             o1 = self.vars_0[row][col]
                             o2 = self.vars_0[neighbor_row][neighbor_col]
-                            V = self.lam
+                            V = self.lambda_smooth
+                            disp = self.d_left[row][col]
                             
                             if max(np.abs(self.left_image[row][col] - self.left_image[neighbor_row][neighbor_col]), \
-                                np.abs(self.right_image[row][col - self.d_left[row][col]] - self.right_image[neighbor_row][neighbor_col - self.d_left[row][col]])) < 8:
-                                V = 3 * self.lam
+                                np.abs(self.right_image[row][col - disp] - self.right_image[neighbor_row][neighbor_col - disp])) < 8:
+                                V = 3 * self.lambda_smooth
 
                             self.add_term2(g, o1, o2, 0, V, V, 0)
                             # self.add_term2(g, o2, o1, 0, V, V, 0)
@@ -192,21 +167,23 @@ class GraphCutsStereo:
                         if self.vars_0[row][col] >= 0 and not self.d_left[row][col] == self.d_left[neighbor_row][neighbor_col] and \
                         neighbor_col >= self.d_left[row][col]:
                             o1 = self.vars_0[row][col]
-                            V = self.lam
+                            V = self.lambda_smooth
+                            disp = self.d_left[row][col]
 
                             if max(np.abs(self.left_image[row][col] - self.left_image[neighbor_row][neighbor_col]), \
-                                np.abs(self.right_image[row][col - self.d_left[row][col]] - self.right_image[neighbor_row][neighbor_col - self.d_left[row][col]])) < 8:
-                                V = 3 * self.lam
+                                np.abs(self.right_image[row][col - disp] - self.right_image[neighbor_row][neighbor_col - disp])) < 8:
+                                V = 3 * self.lambda_smooth
 
                             g.add_tedge(o1, 0, V)
                         elif self.vars_0[neighbor_row][neighbor_col] >= 0 and not self.d_left[row][col] == self.d_left[neighbor_row][neighbor_col] and \
                         col >= self.d_left[neighbor_row][neighbor_col]:
                             o1 = self.vars_0[neighbor_row][neighbor_col]
-                            V = self.lam
+                            V = self.lambda_smooth
+                            disp = self.d_left[neighbor_row][neighbor_col]
 
                             if max(np.abs(self.left_image[row][col] - self.left_image[neighbor_row][neighbor_col]), \
-                                np.abs(self.right_image[row][col - self.d_left[neighbor_row][neighbor_col]] - self.right_image[neighbor_row][neighbor_col - self.d_left[neighbor_row][neighbor_col]])) < 8:
-                                V = 3 * self.lam
+                                np.abs(self.right_image[row][col - disp] - self.right_image[neighbor_row][neighbor_col - disp])) < 8:
+                                V = 3 * self.lambda_smooth
 
                             g.add_tedge(o1, 0, V)
 
@@ -217,24 +194,29 @@ class GraphCutsStereo:
                 if self.vars_0[row][col] >= 0 and self.vars_A[row][col] >= 0:
                     o = self.vars_0[row][col]
                     a = self.vars_A[row][col]
-                    self.add_term2(g, o, a, 0, 1e9, 0, 0)
-                    # self.add_term2(g, a, o, 0, 1e9, 0, 0)
+                    self.add_term2(g, o, a, 0, 1e10, 0, 0)
+                    # self.add_term2(g, a, o, 0, 1e10, 0, 0)
 
                 # Enforce uniqueness in right image
-                if self.vars_0[row][col + self.d_right[row][col]] >= 0 and col + alpha < self.num_cols and \
+                disp_right = self.d_right[row][col]
+                if self.vars_0[row][col + disp_right] >= 0 and col + alpha < self.num_cols and \
                 self.vars_A[row][col + alpha] >= 0:
-                    o = self.vars_0[row][col + self.d_right[row][col]]
+                    o = self.vars_0[row][col + disp_right]
                     a = self.vars_A[row][col + alpha]
-                    self.add_term2(g, o, a, 0, 1e9, 0, 0)
-                    # self.add_term2(g, a, o, 0, 1e9, 0, 0)
+                    self.add_term2(g, o, a, 0, 1e10, 0, 0)
+                    # self.add_term2(g, a, o, 0, 1e10, 0, 0)
 
     def update_d_left_right(self, g, alpha):
         for row in range(self.num_rows):
             for col in range(self.num_cols):
+
+                # Deactivate assignments
                 if self.vars_0[row][col] >= 0 and g.get_segment(self.vars_0[row][col]) == 1:
                     disp = self.d_left[row][col]
                     self.d_left[row][col] = -1
                     self.d_right[row][col - disp] = -1
+                
+                # Activate assignments
                 if self.vars_A[row][col] >= 0 and g.get_segment(self.vars_A[row][col]) == 1:
                     self.d_left[row][col] = alpha
                     self.d_right[row][col - alpha] = alpha
@@ -242,18 +224,22 @@ class GraphCutsStereo:
     def alpha_expansion(self, alpha):
         self.reset_state()
 
+        # Initialize graph
         approx_num_vertices = self.num_rows * self.num_cols
         approx_num_edges = 4 * approx_num_vertices
         g = maxflow.Graph[int](approx_num_vertices, approx_num_edges)
 
+        # Create graph
         nodes = self.add_nodes(g, alpha)
         self.create_data_occlusion_edges(g, alpha)
         self.create_smoothness_edges(g, alpha)
         self.create_unique_edges(g, alpha)
 
+        # Calculate new energy
         max_flow = g.maxflow()
         new_energy = max_flow + self.constant_additional_cut_cost
 
+        # Update state if energy decreases
         if new_energy < self.curr_energy:
             self.curr_energy = new_energy
             print(new_energy)
@@ -266,36 +252,21 @@ class GraphCutsStereo:
         random.shuffle(self.I_disp)
 
         for alpha in self.I_disp:
-            if not self.done[alpha - 1]:
+            if not self.done[alpha]:
                 improved = self.alpha_expansion(alpha)
 
                 if improved:
                     self.done = [False for i in range(len(self.I_disp))]
 
-                self.done[alpha - 1] = True
+                self.done[alpha] = True
 
                 if all(self.done):
                     return
 
     def minimize_energy(self):
-        iterations = 0
         while not all(self.done):
             self.expansion_move()
-            # iterations += 1
-            # if iterations >= 65:
-            #     break
 
-        # Set disparity of occluded pixels to 0
+        # Set disparity of occluded pixels to 0 before returning
         self.d_left[self.d_left == -1] = 0
-        self.d_right[self.d_right == -1] = 0
-
-        # flat_data = self.d_left.flatten()
-        # plt.hist(flat_data, bins=np.arange(min(flat_data), max(flat_data) + 1) - 0.5, edgecolor='black')
-        # plt.xlabel('Values')
-        # plt.ylabel('Frequency')
-        # plt.title('Histogram of 2D NumPy Array')
-        # plt.show()
-
-        # print(self.max_disparity)
-
-        return self.d_left, self.d_right
+        return self.d_left
--- project.py
+++ project.py
@@ -4,14 +4,14 @@ import numpy as np
 import os
 import sys
 
+from matplotlib import pyplot as plt
+from numba import njit, cuda
+
 # I/O directories
-input_dir = "scenes_2006"
+input_dir = "images"
 output_dir = "./"
 
 def get_ssd_disparity_map(left_image, right_image, window_radius):
-    '''
-    Basic SSD window-based correspondence
-    '''
     num_rows, num_cols = left_image.shape[:2]
     window_dimensions = 1 + 2 * window_radius
     max_disparity = int(0.1 * num_cols)
@@ -43,10 +43,37 @@ def get_ssd_disparity_map(left_image, right_image, window_radius):
 
     return disparity_map
 
+@njit(target_backend='cuda')
+def fast_get_ssd_disparity_map(left_image_padded, right_image_padded, num_rows, num_cols, window_radius):
+    window_dimensions = 1 + 2 * window_radius
+    max_disparity = int(0.1 * num_cols)
+    disparity_map = np.zeros((num_rows, num_cols))
+
+    left = left_image_padded
+    right = right_image_padded
+
+    # Determine best correspondence for each pixel
+    for row in range(num_rows):
+        for col in range(num_cols):
+            template_window = left[row:row + window_dimensions, col:col + window_dimensions]
+            min_ssd = -1
+            disparity = 0
+
+            # Search only along horizontal epipolar line
+            for epipolar_col in range(max(0, col - max_disparity), col):
+                proposed_window = right[row:row + window_dimensions, epipolar_col:epipolar_col + window_dimensions]
+                ssd = np.sum((template_window - proposed_window) ** 2)
+        
+                if min_ssd == -1 or ssd < min_ssd:
+                    min_ssd = ssd
+                    disparity = np.abs(col - epipolar_col)
+
+            # Set disparity between pixel and its correspondent
+            disparity_map[row][col] = disparity
+
+    return disparity_map
+
 def get_gc_disparity_map(left_image, right_image):
-    '''
-    Energy minimization via graph cuts method
-    '''
     gc_stereo_method = graph_cuts_stereo.GraphCutsStereo(left_image, right_image)
     disparity_map = gc_stereo_method.minimize_energy()
 
@@ -64,7 +91,7 @@ if __name__ == '__main__':
 
     # Resize images (so runtimes fall within a reasonable range)
     num_rows, num_cols = left_image.shape[:2]
-    resized_now_rows = 150      # Restrict all images to have 200 rows
+    resized_now_rows = 250      # Restrict all images to have 200 rows
     resized_num_cols = int((resized_now_rows / num_rows) * num_cols)
     resized_left_image = cv2.resize(left_image, (resized_num_cols, resized_now_rows))
     resized_right_image = cv2.resize(right_image, (resized_num_cols, resized_now_rows))
@@ -75,18 +102,42 @@ if __name__ == '__main__':
     if run_ssd:
         # Calculate SSD disparity map
         window_radius = 4
-        ssd_disp_map = get_ssd_disparity_map(resized_left_image, resized_right_image, window_radius)
-        ssd_disp_map = cv2.normalize(ssd_disp_map, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+        # ssd_disp_map = get_ssd_disparity_map(resized_left_image, resized_right_image, window_radius)
+        # ssd_disp_map = cv2.normalize(ssd_disp_map, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
 
-        # Save SSD disparity map
+        # # Save SSD disparity map
+        # cv2.imwrite(os.path.join(output_dir, scene_name + '_ssd_disp.png'), ssd_disp_map)
+
+
+
+
+
+
+        # Preprocess image
+        num_rows, num_cols = left_image.shape[:2]
+        left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY).astype(int)
+        left_image_padded = cv2.copyMakeBorder(left_image, window_radius, window_radius, window_radius, window_radius, cv2.BORDER_REFLECT_101)
+        right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2GRAY).astype(int)
+        right_image_padded = cv2.copyMakeBorder(right_image, window_radius, window_radius, window_radius, window_radius, cv2.BORDER_REFLECT_101)
+
+        ssd_disp_map = fast_get_ssd_disparity_map(left_image_padded, right_image_padded, num_rows, num_cols, window_radius)
+        ssd_disp_map = cv2.normalize(ssd_disp_map, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
         cv2.imwrite(os.path.join(output_dir, scene_name + '_ssd_disp.png'), ssd_disp_map)
+
+
+
 
     if run_gc:
         # Calculate graph cut disparity map
-        gc_disp_map_left, gc_disp_map_right = get_gc_disparity_map(resized_left_image, resized_right_image)
+        gc_disp_map_left = get_gc_disparity_map(resized_left_image, resized_right_image)
+        l = gc_disp_map_left
         gc_disp_map_left = cv2.normalize(gc_disp_map_left, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
-        gc_disp_map_right = cv2.normalize(gc_disp_map_right, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
 
         # Save graph cut disparity map
-        cv2.imwrite(os.path.join(output_dir, scene_name + '_gc_disp_left.png'), gc_disp_map_left)
-        cv2.imwrite(os.path.join(output_dir, scene_name + '_gc_disp_right.png'), gc_disp_map_right)
+        cv2.imwrite(os.path.join(output_dir, scene_name + '_gc_disp.png'), gc_disp_map_left)
+
+        plt.imshow(gc_disp_map_left, cmap="jet")
+        plt.show()
+
+        plt.imshow(l, cmap="jet")
+        plt.show()
